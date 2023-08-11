ab6a6595a89e1ca0e897d912cbbe4b6ab6f1da86
Kenneth Kang <kkang@paypal.com>
2023-08-11 11:14:37 -0700

    fix coverage merge

--- tests/unittest/allcover.sh
+++ tests/unittest/allcover.sh
@@ -1,21 +1,28 @@
-export GOROOT=/home/runner/work/hera2flow/hera2flow/go
-mkdir -p /home/runner/go/src/github.com/paypal
-ln -s /home/runner/work/hera2flow/hera2flow /home/runner/go/src/github.com/paypal/hera
-export GOPATH=/home/runner/go
+tarball=go1.20.7.linux-amd64.tar.gz
+wget -nv https://go.dev/dl/$tarball
+echo f0a87f1bcae91c4b69f8dc2bc6d7e6bfcd7524fceec130af525058c0c17b1b44  $tarball | sha256sum -c
+if [ x$? != x0 ]
+then
+    echo bad sha256sum
+    exit 1
+fi
+tar zxf $tarball
 
-rm -rf $GOPATH/allcover
+export GOROOT=$PWD/go
+export GOPATH=/home/runner/go
+mkdir -p $GOPATH/src/github.com/paypal
+ln -s $PWD $GOPATH/src/github.com/paypal/hera
+
+rm -rf $GOPATH/allcover{,2}
 mkdir $GOPATH/allcover
 
 $GOROOT/bin/go install -cover github.com/paypal/hera/worker/mysqlworker
-ls -l $GOPATH/bin
-pwd
 
 overall=0
 for d in `ls -F tests/unittest | grep /$ | sed -e "s,/,," | egrep -v '(mysql_recycle|log_checker_initdb|testutil|rac_maint|mysql_direct|failover)'`
 do 
     echo ==== $d
-    pushd tests/unittest/$d 
-    #cp /home/runner/go/bin/mysqlworker .
+    cd tests/unittest/$d 
     cp $GOPATH/bin/mysqlworker .
     rm -f *.log 
 
@@ -23,32 +30,19 @@ do
     $GOROOT/bin/go build -cover github.com/paypal/hera/tests/unittest/$d
     mkdir integcov
     GOCOVERDIR=integcov ./$d
+    rv=$?
+    echo rv rv $rv for test $d under integration coverage run
     $GOROOT/bin/go tool covdata percent -i=integcov
-    $GOROOT/bin/go tool covdata merge -i=integcov,$GOPATH/allcover -o $GOPATH/allcover
+    mkdir $GOPATH/allcover2
+    $GOROOT/bin/go tool covdata merge -i=integcov,$GOPATH/allcover -o $GOPATH/allcover2
+    rm -rf $GOPATH/allcover
+    mv $GOPATH/allcover{2,}
 
-    #$GOROOT/bin/go test -c github.com/paypal/hera/tests/unittest/$d 
-    #./$d.test -test.v
-    #rv=$?
-#    grep -E '(FAIL|PASS)' -A1 *.log
-#    if [ 0 != $rv ]
-#    then
-#        echo "Retrying" $d
-#        echo "exit code" $rv 
-#        ./$d.test -test.v
-#        rv=$?
-#        grep -E '(FAIL|PASS)' -A1 *.log
-#    fi
-#    if [ 0 != $rv ]
-#    then
-#        #grep ^ *.log
-#        popd
-#        #exit $rv
-#        overall=1
-#        continue
-#    fi
     rm -f *.log 
-    popd
+    cd ../../..
 done
 $GOROOT/bin/go tool covdata func -i=$GOPATH/allcover
 $GOROOT/bin/go tool covdata percent -i=$GOPATH/allcover
+$GOROOT/bin/go tool covdata textfmt -i=$GOPATH/allcover -o $GOPATH/allcover.out
+$GOROOT/bin/go tool cover -html=$GOPATH/allcover.out -o $GOPATH/allcover.htm
 exit $overall
